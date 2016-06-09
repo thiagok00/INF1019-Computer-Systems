abaa56ec16f71ca432202730491dc97e292ca8f1
THIAGO KLEIN DE ANGELIS <c1321929@tigre.grad.inf.puc-rio.br>
2016-06-09 12:07:49 -0300

    LAB8

---
+++ LAB8/LAB8-EX1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#define NUM_THREADS 2
+
+void *contCrescente (void *threadid)  {
+	int i;
+	for (i = 1; i <=20; i++) {
+		printf("%d\n",i);
+		sleep(1);
+	}
+        pthread_exit(NULL); 
+}
+void *contDecrescente (void *threadid)  {
+	int i;
+	for (i = 30; i >=1; i--) {
+		printf("%d\n",i);
+		sleep(2);
+	}
+	pthread_exit(NULL);
+}
+
+int main (void) {
+
+	pthread_t threads[NUM_THREADS];  
+
+	pthread_create(&threads[0], NULL, contCrescente, (void *)0); 
+	pthread_create(&threads[1], NULL, contDecrescente, (void *)1); 
+
+	pthread_join(threads[0],NULL);
+	pthread_join(threads[1],NULL);
+
+	return 0;
+}
---
+++ LAB8/LAB8-EX3.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#define NUM_THREADS 2
+#define N 8
+
+// gcc -Wall -o LAB8-EX3 LAB8-EX3.c -l pthread
+
+
+struct fila {
+	int n;
+	int ini;
+	int vet[N];
+};
+typedef struct fila Fila;
+
+
+static Fila *f;
+
+
+Fila* fila_cria (void) {
+	Fila* f = (Fila*) malloc(sizeof(Fila));
+	f->n = 0;     
+	f->ini = 0;
+	return f;
+}
+int fila_vazia(Fila *f) {
+	if (f->n == 0)
+		return 1;
+	return 0;
+}
+
+void fila_insere (Fila* f, int v) {
+	int fim;
+	if (f->n == N) {   
+		/* fila cheia: capacidade esgotada */
+		printf("Capacidade da fila estourou.\n");
+		exit(1);         
+		/* aborta programa */
+	}
+	/* insere elemento na próxima posição livre */
+	fim = (f->ini + f->n) % N;
+	f->vet[fim] = v;
+	f->n++;
+}
+int fila_retira (Fila* f){ 
+	int v;
+	if (fila_vazia(f)) {
+		printf("Fila vazia.\n");
+		exit(1);         
+		/* aborta programa */
+	}
+	/* retira elemento do início */
+	v = f->vet[f->ini];
+	f->ini = (f->ini + 1) % N;
+	f->n--;
+	return v;
+}
+
+void *produtor (void *threadid)  {
+	int i;
+
+	for (i = 1; i <=64;) {
+		int random;
+		srand(time(NULL));
+		random = rand() % 64;
+		if (f->n >= N) {
+			sleep(1);
+			continue;
+		}
+		printf("PRODUZI: %d\n",random);
+		fila_insere(f,random);
+		sleep(1);
+		i++;
+		
+	}
+        pthread_exit(NULL); 
+}
+void *consumidor (void *threadid)  {
+	int i;
+	for (i = 30; i >=1;) {
+		if (f->n == 0) {
+			sleep(2);
+			continue;		
+		}
+		printf("%d\n",fila_retira(f));
+		sleep(2);
+		i--;
+	}
+	pthread_exit(NULL);
+}
+
+
+
+int main (void) {
+
+	pthread_t threads[NUM_THREADS];  
+	f = fila_cria();
+	pthread_create(&threads[0], NULL, produtor, (void *)0); 
+	pthread_create(&threads[1], NULL, consumidor, (void *)1); 
+
+	pthread_join(threads[0],NULL);
+	pthread_join(threads[1],NULL);
+
+
+	return 0;
+}
